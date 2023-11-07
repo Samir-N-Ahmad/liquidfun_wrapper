2ffd0ece07b84cd6ebff30ab1c79fc35ce12e6bc
Samir Ahmad <samir.ahmad@medicus.ai>
2023-11-07 20:38:46 +0300

    add a testing target

---
+++ tests/main.cpp
@@ -0,0 +1,12 @@
+
+
+
+#include <iostream>
+#include "LiquidFunAPI.h"
+
+int main(){
+    LiquidFunEngine* engine = new LiquidFunEngine();
+    engine->initalize();
+    std::cout << "main";
+    return 0;
+}
